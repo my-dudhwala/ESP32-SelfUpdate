edb0f7a43ce89170c524aeb0bb50d3aa4c78769a
M.Y. Dudhwala <shaikhmohammedyasar7@gmail.com>
2023-08-22 11:02:35 +0530

    pio ini change

--- .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
- C:/Users/LC-124-145/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/LC-124-145/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/LC-124-145/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/LC-124-145/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/LC-124-145/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
